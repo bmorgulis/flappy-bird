495e4c133f83132e6ee4b88aefd3bd842276b902
Aharon Seidman <aharondov1@gmail.com>
2024-12-30 18:51:17 -0500

    old things

--- Player.cs
+++ Player.cs
@@ -17,8 +17,8 @@ public partial class Player : CharacterBody2D
         {
             velocity.Y += Gravity * (float)delta;
             velocity.Y = Mathf.Min(velocity.Y, MaxFallSpeed); // Cap fall speed
-
-            velocity.X += Speed;
+        
+            // velocity.X += Speed;
         }
 
         // // Horizontal movement
