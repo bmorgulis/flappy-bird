4bfcfad3df627defa81b19386543bb18484ea4d2
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2025-01-05 16:50:41 -0500

    Get Score

--- Pipe.cs
+++ Pipe.cs
@@ -4,15 +4,21 @@ using System;
 public partial class Pipe : Node2D
 {
 	[Export] private float Speed = 200f;
-
+	private static int _score = 0; // Keep track of the score
+	private Label _scoreLabel; // Reference to the score label
 	public override void _Ready()
 	{
+		
 		var topPipeArea = GetNode<Area2D>("TopPipeArea"); //get the top pipe area
 		var bottomPipeArea = GetNode<Area2D>("BottomPipeArea"); //get the bottom pipe area
-
+		var area = GetNode<Area2D>("Area2D");
+		
 		//connect the pipes to the body entered signal. If player(body) enters the pipe(collision), the OnBodyEntered method will be called
 		topPipeArea.Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
 		bottomPipeArea.Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+		area.Connect("body_entered", new Callable(this, nameof(OnBirdPassed)));
+		
+		_scoreLabel = GetNode<Label>("/root/World/ScoreLabel"); // Adjust path as necessary
 	}
 
 
@@ -30,7 +36,16 @@ public partial class Pipe : Node2D
 	{
 		if (body is Player)
 		{
+			//Todo Zerach SAVE THE SCORE HERE
+			_score = 0; // Reset the score
 			GameManager.Instance.GameOver();
 		}
+	}
+	
+	private void OnBirdPassed(Node body)
+	{
+		_score += 1; // Increment the score
+		_scoreLabel.Text = $"Score: {_score}";
+		GD.Print("Bird passed through the pipe! Score: " + _score);
 	}
 }
